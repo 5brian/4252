8001246197c0c21efa4c0ec753f2a09cb403cfc9
brian <brian.tan88@gmail.com>
2025-02-03 14:14:01 -0500

    lab-1

---
+++ lab-1/lab1ex4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double fahrenheit, celsius;
+
+    cout << "Enter fahrenheit temperature: ";
+    cin >> fahrenheit;
+
+    celsius = (fahrenheit - 32) * 5.0 / 9.0;
+
+    cout << "Celsius is: " << celsius << endl;
+
+    return 0;
+}
+
+
